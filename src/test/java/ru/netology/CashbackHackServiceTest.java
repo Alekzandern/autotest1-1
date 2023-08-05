6464dcb5dca4bc5d65217320616640db4c4b3566
Alexzander <Alex.Zander54.ru>
2023-08-05 20:55:28 +0000

    second commit

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+// import org.junit.Test;
+import static org.testng.Assert.assertEquals;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+}
